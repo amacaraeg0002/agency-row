2d576bff021a6b2d8ea078d38d5d9251886ae630
amacaraeg0002 <datmemesislit@gmail.com>
2026-09-14 22:01:53 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "127.0.0.1",
-  },
-  preview: {
-    host: "127.0.0.1",
-  },
 });
